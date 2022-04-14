b694749848175eb59582bd1d6b41850fb517cb71
Carlin Mitchell <carlinmi@umn.edu>
2022-04-14 15:51:39 -0500

    change strategy, now using subprocess and re

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-import uuid
+import subprocess
+import re
 
-print(hex(uuid.getnode()))
+
+
+result = str(subprocess.run('ipconfig /all', capture_output=1))
+
+expression = re.compile(r'(Physical Address(?:\.\s)+:\s(?:[A-a-z0-9][A-a-z0-9]-?){6})')
+physical_addresses = re.findall(expression, result)
+
+clean_addresses = [address.split(':')[1].strip() for address in physical_addresses]
+
+
+
+print(clean_addresses)
